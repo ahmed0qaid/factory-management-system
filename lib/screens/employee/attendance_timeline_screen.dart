d83be9a94ab8c728b15f61b003a0bf4bfb4686e1
AHMED QAID <ahmed0qaid@gmail.com>
2026-09-10 22:06:57 +0300

    refactor: route attendance shortcuts to main attendance tab

--- lib/screens/employee/attendance_timeline_screen.dart
+++ lib/screens/employee/attendance_timeline_screen.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 
-import '../../widgets/common/app_scaffold.dart';
-import 'attendance_screen.dart';
+import '../../services/employee_tab_navigation.dart';
 
-class AttendanceTimelineScreen extends StatelessWidget {
+class AttendanceTimelineScreen extends StatefulWidget {
   const AttendanceTimelineScreen({super.key});
 
   @override
+  State<AttendanceTimelineScreen> createState() => _AttendanceTimelineScreenState();
+}
+
+class _AttendanceTimelineScreenState extends State<AttendanceTimelineScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      EmployeeTabNavigation.openAttendance();
+      if (mounted && Navigator.of(context).canPop()) {
+        Navigator.of(context).pop();
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const AppScaffold(
-      title: 'سجل الحضور والانصراف',
-      body: AttendanceScreen(),
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
     );
   }
 }
